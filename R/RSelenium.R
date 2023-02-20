36ddd0daaa82a8bda84af304ed9589d338babafd
Laura <erdaradun.gaztea+github@gmail.com>
2023-02-20 13:19:46 +0100

    add RSelenium config

---
+++ R/RSelenium.R
@@ -0,0 +1,27 @@
+.REMOTE_DRIVER <- NULL
+
+#' Create browser connection for RSelenium
+#'
+#' @description
+#' Connects to a browser for RSelenium. RSelenium is needed for some cases where
+#' rvest package doesn't suffice, usually due to 403 or 503 status codes
+#' received. However, RSelenium setup is much more complicated, this is why
+#' rvest is preferred unless there is no choice.
+#'
+#' @param ... `ANY`\cr
+#'  Parameters passed to [RSelenium::remoteDriver()].
+#'
+#' @return Nothing, function called for its side effects.
+#'
+#' @examples
+#' \dontrun{
+#' start_remote_driver(port = 4567L, browserName = "chrome")
+#' }
+#'
+#' @importFrom RSelenium remoteDriver
+#' @importFrom utils assignInMyNamespace
+#' @export
+start_remote_driver <- function(...) {
+  assignInMyNamespace(".REMOTE_DRIVER", remoteDriver(...))
+  .REMOTE_DRIVER$open()
+}
