a0e89300b8c8cf0b1b02b1ea741250a965e0fe22
Laura <erdaradun.gaztea+github@gmail.com>
2023-03-05 11:00:55 +0100

    implement what_access()

--- R/is_open_access.R
+++ R/is_open_access.R
@@ -1,44 +1,12 @@
-.CURRENT_DOI <- NULL
-
 #' Check a paper for open access
 #'
 #' @description
 #' Checks whether the full text of the paper is available without restrictions.
 #' Throws error if no interpreter available for the journal.
 #'
-#' @param article `character(1) | xml_document(1)`\cr
-#'  DOI of the paper; or the HTML site containing the paper acquired using
-#'  [rvest::read_html()] or [httr2::resp_body_html()].
+#' @inheritParams what_access
 #'
-#' @section Supported sources:
-#' * Academic Press
-#' * ACS Publications
-#' * Analytical Science Journals
-#' * BioMed Central
-#' * Biophysical Journal
-#' * Cambridge Core
-#' * Cell Reports
-#' * eLife
-#' * Elsevier
-#' * FEBS Press
-#' * Frontiers
-#' * Hindawi
-#' * Journal of Biological Chemistry
-#' * Journal of Neuroscience
-#' * MDPI
-#' * Molecular Cell
-#' * Nature
-#' * OUP Academic
-#' * PeerJ
-#' * Pergamon
-#' * PLOS ONE
-#' * PNAS
-#' * Portland Press
-#' * The Royal Society of Chemistry
-#' * Science
-#' * SpringerLink
-#' * Taylor & Francis
-#' * Wiley Online Library
+#' @inheritSection what_access Supported sources
 #'
 #' @return `TRUE` or `FALSE` depending on check result.
 #'
@@ -46,70 +14,6 @@
 #' is_open_access("10.1093/nar/gkac882")
 #'
 #' @export
-is_open_access <- function(article) {
-  UseMethod("is_open_access")
-}
-
-#' @rdname is_open_access
-#' @importFrom glue glue
-#' @importFrom utils assignInMyNamespace
-#' @export
-is_open_access.character <- function(article) {
-  assignInMyNamespace(".CURRENT_DOI", article)
-  is_open_access(get_html(glue("https://doi.org/{article}")))
-}
-
-#' @rdname is_open_access
-#' @importFrom rvest read_html
-#' @export
-is_open_access.xml_document <- function(article) {
-  redirects <- is_redirect(article)
-  while (redirects) {
-    article <- get_html(attr(redirects, "url", exact = TRUE))
-    redirects <- is_redirect(article)
-  }
-  is_open_access(determine_source(article))
-}
-
-#' @rdname is_open_access
-#' @export
-is_open_access.oa_remote_driver <- function(article) {
-  if (is_overloaded(article)) {
-    do.call(start_remote_driver, .REMOTE_DRIVER_SETTINGS)
-    is_open_access(.CURRENT_DOI)
-  }
-  is_open_access(determine_source(article))
-}
-
-#' @rdname is_open_access
-#' @export
-is_open_access.oa_html_document <- function(article) {
-  check_open_access(article)
-}
-
-#' @importFrom rvest html_attr html_element
-determine_source <- function(page) {
-  source <- page %>%
-    find_element("head>meta[property=\"og:site_name\"]") %>%
-    element_attr("content")
-
-  if (is.na(source) || source == "") {
-    # If `og:site_name` property is missing
-    source <- page %>%
-      find_element("head>meta[name=citation_publisher]") %>%
-      element_attr("content")
-  }
-
-  if (is.na(source) || source == "") {
-    # If citation publisher is missing as well
-    source <- page %>%
-      find_element("head>meta[name=\"DC.publisher\"]") %>%
-      element_attr("content")
-  }
-
-  structure(
-    page,
-    source_site = source,
-    class = c("oa_html_document", class(page))
-  )
+is_open_access <- function(articles) {
+  what_access(articles) == "Open Access"
 }
---
+++ R/what_access.R
@@ -0,0 +1,113 @@
+.CURRENT_DOI <- NULL
+
+#' Check paper access type
+#'
+#' @description
+#' TODO
+#'
+#' @param articles `character() | xml_document()`\cr
+#'  DOIs of the papers; or the HTML sites containing the papers acquired using
+#'  [rvest::read_html()] or [httr2::resp_body_html()].
+#'
+#' @details
+#' # Supported sources
+#'
+#' * Academic Press
+#' * ACS Publications
+#' * Analytical Science Journals
+#' * BioMed Central
+#' * Biophysical Journal
+#' * Cambridge Core
+#' * Cell Reports
+#' * eLife
+#' * Elsevier
+#' * FEBS Press
+#' * Frontiers
+#' * Hindawi
+#' * Journal of Biological Chemistry
+#' * Journal of Neuroscience
+#' * MDPI
+#' * Molecular Cell
+#' * Nature
+#' * OUP Academic
+#' * PeerJ
+#' * Pergamon
+#' * PLOS ONE
+#' * PNAS
+#' * Portland Press
+#' * The Royal Society of Chemistry
+#' * Science
+#' * SpringerLink
+#' * Taylor & Francis
+#' * Wiley Online Library
+#'
+#' @return Paper access type, one of `Open Access`, `Open Archive`,
+#' `Free Access`, and `Closed Access`.
+#'
+#' @examples
+#' what_access("10.1093/nar/gkac882")
+#'
+#' @export
+what_access <- function(articles) {
+  vapply(articles, what_access_intrnl, FUN.VALUE = character(1))
+}
+
+what_access_intrnl <- function(object) {
+  UseMethod("what_access_intrnl")
+}
+
+#' @importFrom glue glue
+#' @importFrom utils assignInMyNamespace
+what_access_intrnl.character <- function(object) {
+  assignInMyNamespace(".CURRENT_DOI", object)
+  what_access_intrnl(get_html(glue("https://doi.org/{object}")))
+}
+
+#' @importFrom rvest read_html
+what_access_intrnl.xml_document <- function(object) {
+  redirects <- is_redirect(object)
+  while (redirects) {
+    object <- get_html(attr(redirects, "url", exact = TRUE))
+    redirects <- is_redirect(object)
+  }
+  what_access_intrnl(determine_source(object))
+}
+
+what_access_intrnl.oa_remote_driver <- function(object) {
+  if (is_overloaded(object)) {
+    do.call(start_remote_driver, .REMOTE_DRIVER_SETTINGS)
+    what_access_intrnl(.CURRENT_DOI)
+  }
+  what_access_intrnl(determine_source(object))
+}
+
+what_access_intrnl.oa_html_document <- function(object) {
+  check_open_access(object)
+}
+
+#' @importFrom rvest html_attr html_element
+determine_source <- function(page) {
+  source <- page %>%
+    find_element("head>meta[property=\"og:site_name\"]") %>%
+    element_attr("content")
+
+  if (is.na(source) || source == "") {
+    # If `og:site_name` property is missing
+    source <- page %>%
+      find_element("head>meta[name=citation_publisher]") %>%
+      element_attr("content")
+  }
+
+  if (is.na(source) || source == "") {
+    # If citation publisher is missing as well
+    source <- page %>%
+      find_element("head>meta[name=\"DC.publisher\"]") %>%
+      element_attr("content")
+  }
+
+  structure(
+    page,
+    source_site = source,
+    class = c("oa_html_document", class(page))
+  )
+}
